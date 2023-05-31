be47a9a63db6c45740aafa971fc86846ef61e53a
J410977004 <93242831+J410977004@users.noreply.github.com>
2023-05-31 15:50:21 +0800

    Add files via upload

---
+++ Main.R
@@ -0,0 +1,7 @@
+
+FinalProject<- list()
+library(readr)
+Final$y2018 <- read_csv("C:\Users\user\Downloads\歷年重大交通事故地點--1110413 (1).csv")
+View(Final$y2018)
+# Loop over filenames to import data
+for (i in 1:length(filenames)) seq_along(filenames)
